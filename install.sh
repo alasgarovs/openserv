9e010626da1955df5d09c371af38df0bab154be3
sabuhi <sabuhi.alasgarli@pm.me>
2026-03-28 01:30:05 +0400

    feat: configure installation

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Create '.openserv' folder in home directory
+mkdir -p ~/.openserv
+
+# Copy 'src/' folder into '~/.openserv'
+cp -r src/. ~/.openserv/
+
+# Print completion message
+echo "Installation completed successfully."
